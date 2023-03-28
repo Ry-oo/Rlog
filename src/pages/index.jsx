73ee67b7d2a7586205019ff0b68b9dc6bd2c1625
ryoo <ryooc072112@gmail.com>
2023-03-28 13:40:10 +0900

    ⚡️ listのstylingを変更しました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,12 +25,12 @@ export default function Home({ blog }) {
         <title>Rlog</title>
       </Head>
       <main>
-        <ul className="mt-3 space-x-3">
+        <ul className="mt-3 space-y-4 py-3.5 min-w-full max-w-md rounded-br-lg overflow-hidden shadow-lg text-center border-2 outline outline-black">
           {blog.map((blog) => (
             <li key={blog.id}>
               <Link
                 href={`/blog/${blog.id}`}
-                className="text-blue-800 hover:text-blue-400 text-xl"
+                className="text-black hover:text-gray-400 text-xl"
               >
                 {blog.title}
               </Link>
