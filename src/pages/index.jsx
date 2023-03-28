70c99bcd8c74f521bbfbe0ed36e5c0463fa541d4
ryoo <ryooc072112@gmail.com>
2023-03-28 17:38:04 +0900

    ⚡️ 個別にcssを当てるようにした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,9 +25,12 @@ export default function Home({ blog }) {
         <title>Rlog</title>
       </Head>
       <main>
-        <ul className="mt-3 space-y-4 py-3.5 min-w-full max-w-md rounded-br-lg overflow-hidden shadow-lg text-center border-2 outline outline-black">
+        <ul>
           {blog.map((blog) => (
-            <li key={blog.id}>
+            <li
+              key={blog.id}
+              className="mt-3 space-y-4 py-3.5 min-w-full max-w-md rounded-br-lg overflow-hidden shadow-lg text-center border-2 outline outline-black"
+            >
               <Link
                 href={`/blog/${blog.id}`}
                 className="text-black hover:text-gray-400 text-xl"
